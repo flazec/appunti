61e7030f97ca73afd6196d5c0e73f0ae25affc90
flazec <zecchinelliflavio2@gmail.com>
2026-05-11 12:35:24 +0200

    un po' di java

--- roam/Main.java
+++ roam/Main.java
@@ -1,6 +1,22 @@
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println((int)2.781828);
+    public class Triangolo extends Forma{
+        private double base;
+        private double altezza;
+        public Triangolo(double base, double altezza){
+            this.base = base;
+            this.altezza = altezza;
+        }
+        public double getBase(){
+            return base;
+        }
+        public double getAltezza()
+            {
+               return altezza;
+            }
+
+    public static void main(String[] args){
+        Triangolo t = null;
     }
+        }
 }
