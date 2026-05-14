72c10af42d5293c9a419d00f1fd930a79065758a
flazec <zecchinelliflavio2@gmail.com>
2026-05-14 18:08:22 +0200

    Fino a interfacce

--- roam/Main.java
+++ roam/Main.java
@@ -1,22 +1,29 @@
 
 public class Main {
-    public class Triangolo extends Forma{
-        private double base;
-        private double altezza;
-        public Triangolo(double base, double altezza){
-            this.base = base;
-            this.altezza = altezza;
-        }
-        public double getBase(){
-            return base;
-        }
-        public double getAltezza()
-            {
-               return altezza;
-            }
 
-    public static void main(String[] args){
-        Triangolo t = null;
+    public static void main(String[] args) {
+        System.out.print("success");
     }
+
+
+    public class Nastro implements SupportoRiscrivibile{
+        private Pellicola pellicola;
+        @Override
+        public void leggi(){
+            attivaTestina();
+            muoviTestina();
         }
+        @Override
+        public void scrivi(){
+            attivaTestina();
+            caricaTestina();
+            muoviTestina();
+            scaricaTestina();
+        }
+        public void attivaTestina(){}
+        public void caricaTestina(){}
+        public void scaricaTestina(){}
+        public void muoviTestina(){}
+    }
+
 }
